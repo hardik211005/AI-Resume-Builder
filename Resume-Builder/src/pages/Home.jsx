d90c89ab924ba48be3b48143b4cac599792cf1a1
hardik211005 <hardikdhameeja2105@gmail.com>
2025-10-17 11:43:42 +0530

    Home Page

---
+++ Resume-Builder/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from '../Components/home/Banner'
+import Hero from '../Components/home/Hero'
+import Features from '../Components/home/Features'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Banner/>
+        <Hero/>
+        <Features/>
+    </div>
+  )
+}
+
+export default Home
